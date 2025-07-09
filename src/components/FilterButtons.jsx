cfe6e9a6f9bac82562eb4200b08ce2e4b9bdfb98
Uditkishore <uditkishor4292@gmail.com>
2025-07-09 18:18:16 +0530

    Added mark all button

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,11 +1,24 @@
 import { useTaskContext } from "../context/TaskContext";
 
 function FilterButtons() {
-    const { filter, setFilter } = useTaskContext();
+    const { tasks, filter, dispatch, setFilter } = useTaskContext();
+
+    const setAllCompleted = () => {
+
+        let updated = tasks.map((e) => ({
+            ...e,
+            completed: true,
+        }));
+
+        dispatch({
+            type: "SET_TASKS",
+            payload: updated
+        });
+    }
 
     return (
         <div className="flex flex-wrap justify-center gap-2 sm:gap-4 mt-6">
-            {["all", "completed", "pending"].map(f => (
+            {["all", "completed", "pending"].map((f) => (
                 <button
                     key={f}
                     onClick={() => setFilter(f)}
@@ -18,7 +31,15 @@ function FilterButtons() {
                     {f[0].toUpperCase() + f.slice(1)}
                 </button>
             ))}
+            <button
+                onClick={setAllCompleted}
+                className="px-4 py-2 rounded-lg font-semibold border text-sm sm:text-base
+      bg-gray-100 text-gray-700 border-gray-300 hover:brightness-105 dark:bg-gray-700 dark:text-white dark:border-gray-500"
+            >
+                Mark All
+            </button>
         </div>
+
     );
 }
 
